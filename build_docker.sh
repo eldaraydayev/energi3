8e5b9c43f991abfc31c8ce9a241ba3634d8fd95c
eldaraydayev <eldar@aydayev.com>
2022-11-01 03:51:27 +0400

    update#2022.11.01-03:51:27

---
+++ build_docker.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget https://s3-us-west-2.amazonaws.com/download.energi.software/releases/energi3/v3.1.3/energi3-v3.1.3-linux-amd64.tgz
+tar zxvf ./energi3-v3.1.3-linux-amd64.tgz
+git pull -v
