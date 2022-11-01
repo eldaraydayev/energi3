34da2543ec4c7e09ebbc7ae58740d33d402b0542
eldaraydayev <eldar@aydayev.com>
2022-11-01 05:02:03 +0400

    update#2022.11.01-05:02:03

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 wget https://s3-us-west-2.amazonaws.com/download.energi.software/releases/energi3/v3.1.3/energi3-v3.1.3-linux-amd64.tgz
 tar zxvf ./energi3-v3.1.3-linux-amd64.tgz
-git pull -v
+
+sudo apt install -y debootstrap docker-compose docker.io
+sudo debootstrap focal linux > /dev/null
+sudo tar -C linux -c . | sudo docker import - linux
