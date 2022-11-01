c9f8c8cf268282fab56a48bc5c2b66cb303739e6
eldaraydayev <eldar@aydayev.com>
2022-11-01 06:14:45 +0400

    update#2022.11.01-06:14:45

--- build_docker.sh
+++ build_docker.sh
@@ -5,5 +5,5 @@ tar zxvf ./energi3-v3.1.3-linux-amd64.tgz
 sudo apt install -y debootstrap docker-compose docker.io
 sudo debootstrap focal linux > /dev/null
 sudo tar -C linux -c . | sudo docker import - linux
-#sudo COMPOSE_PROJECT_NAME=energi ENERGI_VERSION=v3.1.3 STAKER_HOME=/home/nrgstaker ENERGI_BIN=/usr/local/bin/energi ENERGI_CORE_DIR=${STAKER_HOME}/.energi_core docker build --tag energi3 .
 sudo docker build --tag energi3 .
+sudo docker-compose up --detach
