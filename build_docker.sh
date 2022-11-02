6c4be46368b3301066f8c626e44b173db1665e2c
eldaraydayev <eldar@aydayev.com>
2022-11-03 02:50:08 +0400

    update#2022.11.03-02:50:08

--- build_docker.sh
+++ build_docker.sh
@@ -15,6 +15,13 @@ printf "\nExtracting Energi3 TarBall file ......"
 tar zxf ./energi3-v3.1.3-linux-amd64.tgz -C energi3
 printf "DONE\n\n"
 
+printf "Install Docker Security Scanner Trivy ........"
+sudo apt-get install -y wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update -y
+printf "DONE\n\n"
+
 printf "Installing Docker ........"
 
 sudo apt install -y debootstrap docker-compose docker.io
@@ -30,6 +37,9 @@ printf "DONE\n\n"
 printf "Checking Docker images ......."
 sudo docker inspect energi3
 printf ".... DONE"
+
+printf "Cheking Docker image energi3"
+sudo trivy image energi3
 
 echo "Checking the Energi3 Node logs output"
 sudo docker logs -f -n 100 energi3
